68f7c7314e2df2dd65c8c86df4dab21006dd3a6f
mayud <mayudong200333@gmail.com>
2023-03-03 00:01:31 +0900

    added e2e test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
-import { Module,ValidationPipe,MiddlewareConsumer } from '@nestjs/common';
+import { Module, ValidationPipe, MiddlewareConsumer } from '@nestjs/common';
 import { APP_PIPE } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -10,26 +11,43 @@ import { Report } from './reports/report.entity';
 const cookieSession = require('cookie-session');
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type:'sqlite',
-    database:'db.sqlite',
-    entities:[User,Report],
-    synchronize:true,
-  }),UsersModule, ReportsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`
+    }),
+    TypeOrmModule.forRootAsync({
+      inject:[ConfigService],
+      useFactory: (config:ConfigService) =>{
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          synchronize:true,
+          entities: [User,Report]
+        }
+      }
+    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'sqlite',
+    //   database: 'db.sqlite',
+    //   entities: [User, Report],
+    //   synchronize: true,
+    // }), 
+    UsersModule, ReportsModule],
   controllers: [AppController],
   providers: [AppService,
-  {
-    provide: APP_PIPE,
-    useValue: new ValidationPipe({
-      whitelist:true
-    }),
-  }],
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true
+      }),
+    }],
 })
 export class AppModule {
-  configure(consumer:MiddlewareConsumer){
+  configure(consumer: MiddlewareConsumer) {
     consumer.apply(cookieSession({
-      keys:['asdfasdf'],
+      keys: ['asdfasdf'],
     }))
-    .forRoutes('*')
+      .forRoutes('*')
   }
 }
